d870d76b72082843ccfab7e2d22984912e47c5c4
Benjamin-C <bencrall@gmail.com>
2017-11-08 16:52:26 -0800

    Paused: working on rendering questions

--- src/jeopardy/GamePanel.java
+++ src/jeopardy/GamePanel.java
@@ -1,24 +1,66 @@
 package jeopardy;
 
-import java.awt.Component;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.util.List;
 
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
-	Graphics myGraphics;
-	GamePanel() {
+public class GamePanel{
+	
+	JFrame jf;
+	
+	public GamePanel(JFrame j) {
+		jf = j;
 	}
 	
-	public void setGraphics(Graphics g) {
-		myGraphics = g;
-		paintComponent(getGraphics());
-	}
-	
-	@Override
-	public void paintComponent(Graphics g) {
-		g = myGraphics;
+	public boolean drawMainPanel(List<Category> roundOne) {
+		JPanel panel = new JPanel() {
+        	@Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.BLACK);
+                g.fillRect(0,  0,  getWidth(), getHeight());
+                g.setColor(Color.BLUE);
+                String text = "";
+                int sw;
+                int sh = g.getFontMetrics().getHeight();
+                int x;
+                int y;
+                int bw;
+                int bh;
+                g.setFont(new Font(g.getFont().getName(), Font.PLAIN, 20));
+                for(int i = 0; i < roundOne.size(); i++) {
+                	g.setColor(Color.RED);
+                	text = roundOne.get(i).getName();
+                	g.fillRect((getWidth() / 6) * i, 0, (getWidth() / 6) - 10, (getHeight() / 6) - 10);
+                	sw = g.getFontMetrics().stringWidth(text);
+            		sh = g.getFontMetrics().getHeight();
+            		bw = getWidth() / 6;
+            		x = (bw * i) + ((bw - sw) / 2);
+            		y = getHeight() / 6;
+            		y = ((y - sh) / 2);
+            		g.setColor(Color.WHITE);
+            		g.drawString(text, x, y);
+                	for(int j = 0; j < roundOne.get(i).size(); j++) {
+                		g.setColor(Color.BLUE);
+                		g.fillRect((getWidth() / 6) * i, (getHeight() / 6) * (j + 1), (getWidth() / 6) - 10, (getHeight() / 6) - 10);
+                		text = roundOne.get(i).getQuestion(j).getScore() + "";
+                		sw = g.getFontMetrics().stringWidth(text);
+                		bw = getWidth() / 6;
+                		x = (bw * i) + ((bw - sw) / 2);
+                		bh = getHeight() / 6;
+                		y = (bh * (j + 1)) + ((bh - sh) / 2);
+                		g.setColor(Color.WHITE);
+                		g.drawString(text, x, y);
+                	}
+                }
+            }
+        };
+        jf.add(panel);
+        jf.validate();
+        return true;
 	}
 }
--- src/jeopardy/Main.java
+++ src/jeopardy/Main.java
@@ -3,16 +3,20 @@ package jeopardy;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Main{
 	
 	public static void main(String args[]) {
+		
+		
 		List<Category> roundOne = new ArrayList<Category>();
 		String names[] = {"cooking", "fire", "firstaid", "knives", "scoutstuff", "water"};
 		for(int i = 0; i < names.length; i++) {
@@ -27,60 +31,49 @@ public class Main{
 		jf.setSize(1280, 720);
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-		JPanel panel;
+		jf.addKeyListener(new KeyListener() {
+			@Override public void keyTyped(KeyEvent e) {KeyListen.keyTyped(e);}
+			@Override public void keyReleased(KeyEvent e) {KeyListen.keyReleased(e);}
+			@Override public void keyPressed(KeyEvent e) {KeyListen.keyPressed(e);}
+		});
 		
-        panel = new JPanel() {
+		GamePanel gp = new GamePanel(jf);
+		gp.drawMainPanel(roundOne);
+		
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		displayText(roundOne.get(1).getQuestion(1).getQuestion(), jf);
+	}
+	
+	public static boolean displayText(String text, JFrame jf) {
+		JPanel panel = new JPanel() {
         	@Override
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.setColor(Color.BLACK);
                 g.fillRect(0,  0,  getWidth(), getHeight());
                 g.setColor(Color.BLUE);
-                String text = "";
-                int sw;
+                g.fillRect(10,  10, getWidth() - 20, getHeight() - 20);
+                g.setColor(Color.BLACK);
+                g.fillRect(20,  20, getWidth() - 40, getHeight() - 40);
+                g.setColor(Color.BLUE);
+                g.fillRect(25,  25, getWidth() - 50, getHeight() - 50);
+                g.setFont(new Font(g.getFont().getName(), Font.PLAIN, 60));
+                int sw = g.getFontMetrics().stringWidth(text);
                 int sh = g.getFontMetrics().getHeight();
-                int x;
-                int y;
-                int bw;
-                int bh;
-                g.setFont(new Font(g.getFont().getName(), Font.PLAIN, 20));
-                for(int i = 0; i < roundOne.size(); i++) {
-                	g.setColor(Color.RED);
-                	text = roundOne.get(i).getName();
-                	g.fillRect((getWidth() / 6) * i, 0, (getWidth() / 6) - 10, (getHeight() / 6) - 10);
-                	sw = g.getFontMetrics().stringWidth(text);
-            		sh = g.getFontMetrics().getHeight();
-            		bw = getWidth() / 6;
-            		x = (bw * i) + ((bw - sw) / 2);
-            		y = getHeight() / 6;
-            		y = ((y - sh) / 2);
-            		g.setColor(Color.WHITE);
-            		g.drawString(text, x, y);
-                	for(int j = 0; j < roundOne.get(i).size(); j++) {
-                		g.setColor(Color.BLUE);
-                		g.fillRect((getWidth() / 6) * i, (getHeight() / 6) * (j + 1), (getWidth() / 6) - 10, (getHeight() / 6) - 10);
-                		text = roundOne.get(i).getQuestion(j).getScore() + "";
-                		sw = g.getFontMetrics().stringWidth(text);
-                		bw = getWidth() / 6;
-                		x = (bw * i) + ((bw - sw) / 2);
-                		bh = getHeight() / 6;
-                		y = (bh * (j + 1)) + ((bh - sh) / 2);
-                		g.setColor(Color.WHITE);
-                		g.drawString(text, x, y);
-                	}
-                }
-            }
-        };
-        jf.add(panel);
+        		int x = ((getWidth() - sw) / 2);
+        		int y = ((getHeight() - sh) / 2);
+        		g.setColor(Color.WHITE);
+        		g.drawString(text, x, y);
+        	}
+		};
+		jf.add(panel);
         jf.validate();
-        
-//        try {
-//			Thread.sleep(5000);
-//			System.exit(1);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+        return true;
 	}
 }
