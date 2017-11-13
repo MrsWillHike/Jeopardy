89ca58cecc2675f7ad6c3a992b62258fe662bfb4
bencrall@gmail.com <bencrall@gmail.com>
2017-11-12 20:24:48 -0800

    Fixed key listeners

--- src/jeopardy/KeyListen.java
+++ src/jeopardy/KeyListen.java
@@ -22,25 +22,27 @@ public class KeyListen {
 		int num = 0;
 		int dig = 0;
 		
+		System.out.println(e.getKeyCode());
 		switch(e.getKeyCode()) {
 		// Select category
 		case KeyEvent.VK_Q: catSel = 0; break;
-		case 'w': catSel = 1; break;
-		case 'e': catSel = 2; break;
-		case 'r': catSel = 3; break;
-		case 't': catSel = 4; break;
-		case 'y': catSel = 5; break;
+		case KeyEvent.VK_W: catSel = 1; break;
+		case KeyEvent.VK_E: catSel = 2; break;
+		case KeyEvent.VK_R: catSel = 3; break;
+		case KeyEvent.VK_T: catSel = 4; break;
+		case KeyEvent.VK_Y: catSel = 5; break;
 
 		// Select points
 		case KeyEvent.VK_A: ptSel = 0; break;
-		case 's': ptSel = 1; break;
-		case 'd': ptSel = 2; break;
-		case 'f': ptSel = 3; break;
-		case 'g': ptSel = 4; break;
+		case KeyEvent.VK_S: ptSel = 1; break;
+		case KeyEvent.VK_D: ptSel = 2; break;
+		case KeyEvent.VK_F: ptSel = 3; break;
+		case KeyEvent.VK_G: ptSel = 4; break;
 		
 		// Number Pad Imput Test
 		case KeyEvent.VK_NUMPAD0: dig = dig * 10; break;
 		case KeyEvent.VK_NUMPAD1: dig = dig * 10; break;
+		
 		// Handle right or wrong
 		case KeyEvent.VK_ESCAPE: {
 			GamePanel.drawMainPanel(cat);
